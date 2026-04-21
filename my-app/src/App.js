629e563c2ba9425350faf9dd02091ece415561d3
Schokolalaa <MrSchokoHD@proton.me>
2026-04-21 21:00:44 +0200

    Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import pfp from './pfp.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hii, This is The GitHub page off <a className="App-link" href="https://schokolala.cc" target="_blank" rel="noopener noreferrer">Schokolala.cc</a> :3</h1>
+        
+        <img src={pfp} className="pfp-logo" alt="Pfp" />
+        <p>This is Like a LinkTree</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://schokolala.cc"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Schokolala.cc
         </a>
+        
       </header>
     </div>
   );
 }
+
+
 
 export default App;
